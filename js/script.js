e7a2c43e63d05cad5885b4abb04f27bb67a8ac16
Olga Nadrshina <nadolga.nl@gmail.com>
2023-09-19 18:28:07 +0200

    removeEnd

--- js/script.js
+++ js/script.js
@@ -94,29 +94,9 @@ function setClock(selector, endtime) {
   
 }
 setClock('.timer', deadline);
-
-
-  
+ 
 
 });
 
 
-const deadline = '2023-09-30';
 
-function getTimeRemaining(endtime) {
-  const t = Date.parse(endtime) - Date.parse(new Date()),
-        days = Math.floor(t / (1000 * 60 * 60 * 24)),
-        hours = Math.floor(t / (1000 * 60 * 60) % 24),
-        minutes = Math.floor(t / (1000 * 60) % 60),
-        seconds = Math.floor(t / (1000) % 60);
-
-  return {
-    "total" : t,
-    "days" : days,
-    "hours" : hours,
-    "minutes" :minutes,
-    "seconds" : seconds
-  }      
-}
-
-console.log(getTimeRemaining(deadline));
